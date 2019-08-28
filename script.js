5527b3121876a0b939d5b9ebbc9b2d2f9b3afb5f
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-08-28 13:10:00 +0300

    v.1.2

--- script.js
+++ script.js
@@ -15,60 +15,43 @@
 // b.z.push(4);
 // console.log(a.z); // [1, 2, 3]
 
-// let newObj = Object.defineProperties({}, Object.getOwnPropertyDescriptors(obj));
 
-let cloneObj = function(obj){
-    let newObj = Object.defineProperties({}, Object.getOwnPropertyDescriptors(obj));
-    return (newObj);
-};
+function deepCopy (obj) {
+
+	let newObj = new obj.constructor();
+
+	Object.entries(obj).forEach(([key]) => {
+
+		if( typeof obj[key] === 'object' ){
+
+            newObj[key] = deepCopy(obj[key]);
+            
+		}else{
+
+			Object.defineProperty(newObj, key, Object.getOwnPropertyDescriptor(obj, key));
+		}
+	});
+
+	return newObj;
+}
 
 
-//ПРОВЕРКА:
-////////////////////////////////////////////////////////////////////////////////////
+// ПРОВЕРКА:
+// //////////////////////////////////////////////////////////////////////////////
+
 let man = {
     name : 'Andy',
     age : 43,
     body : {
-        hand : 2,
+        hand : {
+            finger : 5,
+            palm : 1
+        },
         leg : 2
-    }
+    },
+    money : [1,2,3,4,5],
+    hair : 0,
+    pLength : undefined
 };
 
-let newMan = cloneObj(man);
-
-Object.defineProperty(man, 'age', {writable: false});  
-
-let a = Object.getOwnPropertyDescriptor(man, 'age');
-console.log (a);
-
-let b = Object.getOwnPropertyDescriptor(newMan.body, 'hand');
-console.log (b);
-
-let newMan1 = cloneObj(man);
-
-let c = Object.getOwnPropertyDescriptor(newMan1, 'age');
-console.log (c);
-
-////////////////////
-
-let arr = [55, 'asd', ['a','rt', 3], 89, {name : 'Andy', age : 43, body : {hand : 2, leg : 2}}];
-
-let arr1 = cloneObj(arr);
-
-let d = Object.getOwnPropertyDescriptor(arr1[4], 'age');
-console.log (d);
-
-Object.defineProperty(arr[4], 'age', {writable: false});
-Object.defineProperty(arr[4].body, 'hand', {writable: false});
-
-let i = Object.getOwnPropertyDescriptor(arr1[4], 'age');
-console.log (i);
-
-let arr2 = cloneObj(arr);
-
-let f = Object.getOwnPropertyDescriptor(arr2[4], 'age');
-console.log (f);
-
-let h = Object.getOwnPropertyDescriptor(arr2[4].body, 'hand');
-console.log (h);
-//////////////////////////////////////////////////////////////////
+let newMan = deepCopy(man);
